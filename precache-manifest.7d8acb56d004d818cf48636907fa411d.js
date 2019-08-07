6d1f24048a16cf428910ef2093673ff9a38923cd
kale <intharaksak@gmail.com>
2019-08-06 21:58:32 -0400

    Updates

---
+++ precache-manifest.7d8acb56d004d818cf48636907fa411d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2391672620ad4665c38f07a42df4b4c",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "c31fbcb0eaa8a49d9038",
+    "url": "/Clicky-Game/static/css/main.a387c238.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Clicky-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "c31fbcb0eaa8a49d9038",
+    "url": "/Clicky-Game/static/js/main.2b02419e.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
